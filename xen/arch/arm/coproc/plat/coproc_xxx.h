b978c3aa068886491853ac48baed1b219e28a86a
Oleksandr Tyshchenko <olekstysh@gmail.com>
2016-11-23 12:53:59 +0200

    arm/coproc: Add initial draft for dummy remote processor coproc_xxx
    Signed-off-by: Oleksandr Tyshchenko <olekstysh@gmail.com>

---
+++ xen/arch/arm/coproc/plat/coproc_xxx.h
@@ -0,0 +1,35 @@
+/*
+ * xen/arch/arm/coproc/plat/coproc_xxx.h
+ *
+ * COPROC_XXX platform specific header
+ *
+ * Oleksandr Tyshchenko <Oleksandr_Tyshchenko@epam.com>
+ * Copyright (C) 2016 EPAM Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __ARCH_ARM_COPROC_PLAT_COPROC_XXX_H__
+#define __ARCH_ARM_COPROC_PLAT_COPROC_XXX_H__
+
+#include "../coproc.h"
+#include "common.h"
+
+#endif /* __ARCH_ARM_COPROC_PLAT_COPROC_XXX_H__ */
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
