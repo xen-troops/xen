a647331f04879a35ab31f22164a28f1d46e78f47
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2017-02-02 11:59:05 +0200

    HACK: Update Xen build system
    This is from Yocto build to allow Xen build with
    sysroot populated by Yocto with populate_sdk.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- config/StdGNU.mk
+++ config/StdGNU.mk
@@ -1,15 +1,15 @@
 AS         = $(CROSS_COMPILE)as
-LD         = $(CROSS_COMPILE)ld
+LD         ?= $(CROSS_COMPILE)ld
 ifeq ($(clang),y)
-CC         = $(CROSS_COMPILE)clang
-CXX        = $(CROSS_COMPILE)clang++
+CC         ?= $(CROSS_COMPILE)clang
+CXX        ?= $(CROSS_COMPILE)clang++
 LD_LTO     = $(CROSS_COMPILE)llvm-ld
 else
-CC         = $(CROSS_COMPILE)gcc
-CXX        = $(CROSS_COMPILE)g++
+CC         ?= $(CROSS_COMPILE)gcc
+CXX        ?= $(CROSS_COMPILE)g++
 LD_LTO     = $(CROSS_COMPILE)ld
 endif
-CPP        = $(CC) -E
+CPP        ?= $(CC) -E
 AR         = $(CROSS_COMPILE)ar
 RANLIB     = $(CROSS_COMPILE)ranlib
 NM         = $(CROSS_COMPILE)nm
