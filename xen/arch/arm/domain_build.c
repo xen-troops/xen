64ac4b5c1abaacd677815f43cd5f3d4f6310a35f
Andrii Anisov <andrii_anisov@epam.com>
2017-02-23 19:56:24 +0200

    Revert "arm/coproc: Fix device pass through"
    This reverts commit d2ee2ed9d3c01b811fe1fa8b1e09e35382ece30d.

--- xen/arch/arm/domain_build.c
+++ xen/arch/arm/domain_build.c
@@ -1332,8 +1332,7 @@ static int handle_node(struct domain *d, struct kernel_info *kinfo,
         return make_timer_node(d, kinfo->fdt, node);
 
 #ifdef CONFIG_HAS_COPROC
-    if ( (device_get_class(node) == DEVICE_COPROC) &&
-         (dt_device_used_by(node) == DOMID_XEN) )
+    if ( device_get_class(node) == DEVICE_COPROC )
     {
         res = handle_coproc_node(d, node);
         if ( res)
