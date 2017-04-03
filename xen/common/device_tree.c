dfa5bf2ac74eeba2ef94578cfe59c32a5f7bd45d
Andrii Anisov <andrii_anisov@epam.com>
2017-04-03 11:54:23 +0300

    xen/dt: expose dt_unflatten_device_tree for runtime
    SCF will be configured using partial device tree so we need to parse a DomU's
    partial device tree in XEN. For this we should keep unflatten functions in a
    persistent code section, not init.
    In order to avoid memory leakage during unflattening device trees in runtime
    we have to free memory used for the process. So return to upper layers a raw
    memory pointer to let them free it once it is not needed.
    
    Signed-off-by: Andrii Anisov <andrii_anisov@epam.com>

--- xen/common/device_tree.c
+++ xen/common/device_tree.c
@@ -1710,7 +1710,7 @@ int dt_parse_phandle_with_args(const struct dt_device_node *np,
  * @allnextpp: pointer to ->allnext from last allocated device_node
  * @fpsize: Size of the node path up at the current depth.
  */
-static unsigned long __init unflatten_dt_node(const void *fdt,
+static unsigned long unflatten_dt_node(const void *fdt,
                                               unsigned long mem,
                                               unsigned long *p,
                                               struct dt_device_node *dad,
@@ -1955,11 +1955,12 @@ static unsigned long __init unflatten_dt_node(const void *fdt,
  * @fdt: The fdt to expand
  * @mynodes: The device_node tree created by the call
  */
-static void __init __unflatten_device_tree(const void *fdt,
+static void *__unflatten_device_tree(const void *fdt,
                                            struct dt_device_node **mynodes)
 {
     unsigned long start, mem, size;
     struct dt_device_node **allnextp = mynodes;
+    void *ptr;
 
     dt_dprintk(" -> unflatten_device_tree()\n");
 
@@ -1976,7 +1977,11 @@ static void __init __unflatten_device_tree(const void *fdt,
     dt_dprintk("  size is %#lx allocating...\n", size);
 
     /* Allocate memory for the expanded device tree */
-    mem = (unsigned long)_xmalloc (size + 4, __alignof__(struct dt_device_node));
+    ptr = _xmalloc (size + 4, __alignof__(struct dt_device_node));
+    if ( ptr == NULL )
+        goto out;
+
+    mem = (unsigned long)ptr;
 
     ((__be32 *)mem)[size / 4] = cpu_to_be32(0xdeadbeef);
 
@@ -1994,6 +1999,9 @@ static void __init __unflatten_device_tree(const void *fdt,
     *allnextp = NULL;
 
     dt_dprintk(" <- unflatten_device_tree()\n");
+
+out:
+    return ptr;
 }
 
 static void dt_alias_add(struct dt_alias_prop *ap,
@@ -2080,6 +2088,11 @@ void __init dt_unflatten_host_device_tree(void)
 {
     __unflatten_device_tree(device_tree_flattened, &dt_host);
     dt_alias_scan();
+}
+
+void *dt_unflatten_device_tree(void *fdt, struct dt_device_node ** pdt)
+{
+    return __unflatten_device_tree(fdt, pdt);
 }
 
 /*
