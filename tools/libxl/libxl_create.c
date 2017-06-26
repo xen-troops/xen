c7a40a4b93f9d4d625620a81d3615a329e642cc1
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2017-06-26 14:36:41 +0300

    libxl: add vdispl device
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Acked-by: Wei Liu <wei.liu2@citrix.com>

--- tools/libxl/libxl_create.c
+++ tools/libxl/libxl_create.c
@@ -1444,6 +1444,7 @@ const struct libxl_device_type *device_type_tbl[] = {
     &libxl__usbdev_devtype,
     &libxl__pcidev_devtype,
     &libxl__dtdev_devtype,
+    &libxl__vdispl_devtype,
     NULL
 };
 
