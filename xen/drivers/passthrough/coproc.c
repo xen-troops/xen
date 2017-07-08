8eb8bf06f69d479c6f70cc0ec934b555e744d29f
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2017-07-08 15:54:18 +0300

    iommu: Introduce new APIs and platform ops to handle coproc device
    It was considered to keep two sets of API+callback:
    1. assign_coproc/deassign_coproc
    2. disable_coproc/enable_coproc
    
    First set is intended to be called during vcoproc initialization/destruction
    for doing all required work needed for the following IOMMU switching in runtime,
    etc, therefore it may contain memory allocation, busy loops, etc.
    Second set is intended to be called during vcoproc context switch only and
    as the result it must be as small as possible and can be called from
    the interrupt context.
    
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

---
+++ xen/drivers/passthrough/coproc.c
@@ -0,0 +1,87 @@
+/*
+ * Code to provide SCF ability to manipulate IOMMU
+ *
+ * Oleksandr Tyshchenko <Oleksandr_Tyshchenko@epam.com>
+ * Copyright (C) 2017 EPAM Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <xen/lib.h>
+#include <xen/sched.h>
+#include <xen/guest_access.h>
+#include <xen/iommu.h>
+#include <xsm/xsm.h>
+
+#include "../../arch/arm/coproc/coproc.h"
+
+int iommu_assign_coproc(struct domain *d, device_t *dev)
+{
+    const struct domain_iommu *hd = dom_iommu(d);
+    int rc;
+
+    if ( !iommu_enabled || !hd->platform_ops ||
+         !hd->platform_ops->assign_coproc )
+        return -EINVAL;
+
+    if ( !coproc_is_protected(dev) )
+        return -EINVAL;
+
+    if ( need_iommu(d) <= 0 )
+    {
+        /*
+         * The hwdom is forced to use IOMMU for protecting assigned
+         * device. Therefore the IOMMU data is already set up.
+         */
+        ASSERT(!is_hardware_domain(d));
+        rc = iommu_construct(d);
+        if ( rc )
+            return rc;
+    }
+
+    return hd->platform_ops->assign_coproc(d, dev);
+}
+
+int iommu_deassign_coproc(struct domain *d, device_t *dev)
+{
+    const struct domain_iommu *hd = dom_iommu(d);
+
+    if ( !iommu_enabled || !hd->platform_ops ||
+         !hd->platform_ops->deassign_coproc )
+        return -EINVAL;
+
+    if ( !coproc_is_protected(dev) )
+        return -EINVAL;
+
+    return hd->platform_ops->deassign_coproc(d, dev);
+}
+
+int iommu_disable_coproc(struct domain *d, device_t *dev)
+{
+    const struct domain_iommu *hd = dom_iommu(d);
+
+    if ( !iommu_enabled || !hd->platform_ops ||
+         !hd->platform_ops->disable_coproc )
+        return -EINVAL;
+
+    return hd->platform_ops->disable_coproc(d, dev);
+}
+
+int iommu_enable_coproc(struct domain *d, device_t *dev)
+{
+    const struct domain_iommu *hd = dom_iommu(d);
+
+    if ( !iommu_enabled || !hd->platform_ops ||
+         !hd->platform_ops->enable_coproc )
+        return -EINVAL;
+
+    return hd->platform_ops->enable_coproc(d, dev);
+}
