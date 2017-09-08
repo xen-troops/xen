9d8b52a9e479cae9806981e3abc923c579027178
Oleksandr Dmytryshyn <oleksandr.dmytryshyn@globallogic.com>
2017-09-08 16:15:20 +0300

    cpufreq: make cpufreq driver more generalizable
    First implementation of the cpufreq driver has been
    written with x86 in mind. This patch makes possible
    the cpufreq driver be working on both x86 and arm
    architectures.
    
    This is a rebased version of the original patch:
    https://lists.xen.org/archives/html/xen-devel/2014-11/msg00932.html
    
    Signed-off-by: Oleksandr Dmytryshyn <oleksandr.dmytryshyn@globallogic.com>
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

--- xen/include/xen/acpi.h
+++ xen/include/xen/acpi.h
@@ -123,7 +123,11 @@ static inline int acpi_boot_table_init(void)
 
 #endif 	/*!CONFIG_ACPI*/
 
+#ifdef CONFIG_ACPI
 int get_cpu_id(u32 acpi_id);
+#else
+static inline int get_cpu_id(u32 acpi_id) { return acpi_id; }
+#endif
 
 unsigned int acpi_register_gsi (u32 gsi, int edge_level, int active_high_low);
 int acpi_gsi_to_irq (u32 gsi, unsigned int *irq);
