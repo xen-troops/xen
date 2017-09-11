3c759a4224163418e99722f56983a8cdde185c19
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2017-09-11 09:59:01 +0300

    arm/coproc: Add API to map/unmap partial MMIO range
    Allow drivers to partially map/unmap MMIOs: this is needed
    when only part of the MMIO range is needed, e.g. when
    only set of registers needs to be trapped.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Reviewed-by: Andrii Anisov <andrii_anisov@epam.com>

--- xen/arch/arm/coproc/coproc.h
+++ xen/arch/arm/coproc/coproc.h
@@ -189,6 +189,8 @@ struct coproc_device * coproc_alloc(struct dt_device_node *,
                                     const struct coproc_ops *, u32);
 int coproc_register(struct coproc_device *);
 void coproc_release(struct coproc_device *);
+void *coproc_map_offset(struct coproc_device *, u32 , u64 , u64 );
+void coproc_unmap(void *va);
 struct vcoproc_instance *coproc_get_vcoproc(struct domain *,
                                             struct coproc_device *);
 int vcoproc_domain_init(struct domain *);
