cb6a65ddf5a71c1803768f0aef045d51cee62f19
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2017-10-24 13:52:48 +0300

    xen/arm: Add Xen changes to mailbox infrastructure
    Modify the direct ported mailbox infrastructure to be
    functional inside Xen.
    
    Include "wrappers.h" which contains all required things the direct
    ported code relies on.
    
    Important note: the usage of dummy "wait-for-completion" based on
    busy loop restricts us from using timer based polling.
    So, prevent mailbox controllers (which need polling timer involved)
    from being registered.
    
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

--- xen/arch/arm/cpufreq/mailbox.h
+++ xen/arch/arm/cpufreq/mailbox.h
@@ -2,6 +2,11 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
+ *
+ * Based on Linux drivers/mailbox/mailbox.h
+ * => commit 86c22f8c9a3b71d42d38bfcd80372de72f573713
+ *
+ * No Xen modification.
  */
 
 #ifndef __MAILBOX_H
@@ -12,3 +17,12 @@
 #define TXDONE_BY_ACK	BIT(2) /* S/W ACK recevied by Client ticks the TX */
 
 #endif /* __MAILBOX_H */
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 8
+ * indent-tabs-mode: t
+ * End:
+ */
