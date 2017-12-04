e2bd7610e736050e0c488d2ced8d9489e5b57acd
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2017-12-04 18:11:12 +0200

    pmstat: make pmstat functions more generalizable
    ACPI-specific parts are moved under appropriate ifdefs.
    Now pmstat functions can be used in ARM platform.
    
    This is a rebased version of the original patch:
    https://lists.xen.org/archives/html/xen-devel/2014-11/msg00941.html
    
    Signed-off-by: Oleksandr Dmytryshyn <oleksandr.dmytryshyn@globallogic.com>
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

--- xen/include/xen/pmstat.h
+++ xen/include/xen/pmstat.h
@@ -6,10 +6,17 @@
 #include <public/sysctl.h>   /* for struct pm_cx_stat */
 
 int set_px_pminfo(uint32_t cpu, struct xen_processor_performance *perf);
+#ifdef CONFIG_ACPI /* or CONFIG_X86 ? */
 long set_cx_pminfo(uint32_t cpu, struct xen_processor_power *power);
 uint32_t pmstat_get_cx_nr(uint32_t cpuid);
 int pmstat_get_cx_stat(uint32_t cpuid, struct pm_cx_stat *stat);
 int pmstat_reset_cx_stat(uint32_t cpuid);
+#else
+static inline long set_cx_pminfo(uint32_t cpu, struct xen_processor_power *power) { return 0; }
+static inline uint32_t pmstat_get_cx_nr(uint32_t cpuid) { return 0; }
+static inline int pmstat_get_cx_stat(uint32_t cpuid, struct pm_cx_stat *stat) { return 0; }
+static inline int pmstat_reset_cx_stat(uint32_t cpuid) { return 0; }
+#endif
 
 int do_get_pm_info(struct xen_sysctl_get_pmstat *op);
 int do_pm_op(struct xen_sysctl_pm_op *op);
