7ff4a0de6803c00d994594df8acf4df857dedf8b
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2018-01-31 21:03:11 +0200

    libxl: add vgsx device
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Reviewed-by: Andrii Anisov <andrii_anisov@epam.com>

--- tools/libxl/libxl_internal.h
+++ tools/libxl/libxl_internal.h
@@ -3638,6 +3638,7 @@ extern const struct libxl_device_type libxl__pcidev_devtype;
 extern const struct libxl_device_type libxl__vdispl_devtype;
 extern const struct libxl_device_type libxl__p9_devtype;
 extern const struct libxl_device_type libxl__vsnd_devtype;
+extern const struct libxl_device_type libxl__vgsx_devtype;
 
 extern const struct libxl_device_type *device_type_tbl[];
 
