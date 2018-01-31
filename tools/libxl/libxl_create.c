d34ff758eac049968351c6a5aa4bb87955e5e069
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2018-01-31 21:01:38 +0200

    libxl: add backend type to vkb
    New field backend_type is added to vkb device
    in order to have QEMU and user space backend
    simultaneously. Each vkb backend shall read
    appropriate XS entry and service only own
    frontends.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- tools/libxl/libxl_create.c
+++ tools/libxl/libxl_create.c
@@ -1369,6 +1369,7 @@ static void domcreate_launch_dm(libxl__egc *egc, libxl__multidev *multidev,
         libxl__device_console_dispose(&console);
 
         libxl_device_vkb_init(&vkb);
+        vkb.backend_type = LIBXL_VKB_BACKEND_QEMU;
         libxl__device_add(gc, domid, &libxl__vkb_devtype, &vkb);
         libxl_device_vkb_dispose(&vkb);
 
@@ -1397,6 +1398,9 @@ static void domcreate_launch_dm(libxl__egc *egc, libxl__multidev *multidev,
         for (i = 0; i < d_config->num_vfbs; i++) {
             libxl__device_add(gc, domid, &libxl__vfb_devtype,
                               &d_config->vfbs[i]);
+        }
+
+        for (i = 0; i < d_config->num_vkbs; i++) {
             libxl__device_add(gc, domid, &libxl__vkb_devtype,
                               &d_config->vkbs[i]);
         }
