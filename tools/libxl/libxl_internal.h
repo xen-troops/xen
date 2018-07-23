206431d294d715df1310b431c7ad5d466e80cb72
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2018-07-23 19:17:57 +0300

    Revert "libxl: add PV sound device"
    This reverts commit 512428cad532485a59d8e8c9f272e0a1d8114f80.

--- tools/libxl/libxl_internal.h
+++ tools/libxl/libxl_internal.h
@@ -3637,7 +3637,6 @@ extern const struct libxl_device_type libxl__usbdev_devtype;
 extern const struct libxl_device_type libxl__pcidev_devtype;
 extern const struct libxl_device_type libxl__vdispl_devtype;
 extern const struct libxl_device_type libxl__p9_devtype;
-extern const struct libxl_device_type libxl__vsnd_devtype;
 extern const struct libxl_device_type libxl__vgsx_devtype;
 
 extern const struct libxl_device_type *device_type_tbl[];
