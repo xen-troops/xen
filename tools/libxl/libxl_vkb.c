e45096aeb5100301511e9bb4e15c91c541f621b1
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2018-08-01 18:06:21 +0300

    Revert "libxl: vkb add list and info functions"
    This reverts commit a45158b731fb1a64259686d0b87ba202534bc313.

--- tools/libxl/libxl_vkb.c
+++ tools/libxl/libxl_vkb.c
@@ -13,7 +13,6 @@
  */
 
 #include "libxl_internal.h"
-#include "xen/io/kbdif.h"
 
 static int libxl__device_vkb_setdefault(libxl__gc *gc, uint32_t domid,
                                         libxl_device_vkb *vkb, bool hotplug)
@@ -26,22 +25,6 @@ static int libxl__device_vkb_dm_needed(libxl_device_vkb *vkb, uint32_t domid)
    if (vkb->backend_type == LIBXL_VKB_BACKEND_QEMU)
         return 1;
     return 0;
-}
-
-static int libxl__vkb_from_xenstore(libxl__gc *gc, const char *libxl_path,
-                                    libxl_devid devid,
-                                    libxl_device_vkb *vkb)
-{
-    const char *be_path;
-    int rc;
-
-    vkb->devid = devid;
-    rc = libxl__xs_read_mandatory(gc, XBT_NULL,
-                                  GCSPRINTF("%s/backend", libxl_path),
-                                  &be_path);
-    if (rc) return rc;
-
-    return libxl__backendpath_parse_domid(gc, be_path, &vkb->backend_domid);
 }
 
 int libxl_device_vkb_add(libxl_ctx *ctx, uint32_t domid, libxl_device_vkb *vkb,
@@ -61,71 +44,18 @@ out:
     return AO_INPROGRESS;
 }
 
-int libxl_device_vkb_getinfo(libxl_ctx *ctx, uint32_t domid,
-                             libxl_device_vkb *vkb,
-                             libxl_vkbinfo *info)
-{
-    GC_INIT(ctx);
-    char *libxl_path, *dompath, *devpath;
-    char *val;
-    int rc;
-
-    libxl_vkbinfo_init(info);
-    dompath = libxl__xs_get_dompath(gc, domid);
-    info->devid = vkb->devid;
-
-    devpath = GCSPRINTF("%s/device/%s/%d", dompath,
-                        libxl__device_kind_to_string(LIBXL__DEVICE_KIND_VKBD),
-                        info->devid);
-    libxl_path = GCSPRINTF("%s/device/%s/%d",
-                           libxl__xs_libxl_path(gc, domid),
-                           libxl__device_kind_to_string(LIBXL__DEVICE_KIND_VKBD),
-                           info->devid);
-    info->backend = xs_read(ctx->xsh, XBT_NULL,
-                            GCSPRINTF("%s/backend", libxl_path),
-                            NULL);
-    if (!info->backend) { rc = ERROR_FAIL; goto out; }
-
-    rc = libxl__backendpath_parse_domid(gc, info->backend, &info->backend_id);
-    if (rc) goto out;
-
-    val = libxl__xs_read(gc, XBT_NULL, GCSPRINTF("%s/state", devpath));
-    info->state = val ? strtoul(val, NULL, 10) : -1;
-
-    info->frontend = xs_read(ctx->xsh, XBT_NULL,
-                             GCSPRINTF("%s/frontend", libxl_path),
-                             NULL);
-    info->frontend_id = domid;
-
-    val = libxl__xs_read(gc, XBT_NULL,
-          GCSPRINTF("%s/"XENKBD_FIELD_EVT_CHANNEL, devpath));
-    info->evtch = val ? strtoul(val, NULL, 10) : -1;
-
-    val = libxl__xs_read(gc, XBT_NULL,
-          GCSPRINTF("%s/"XENKBD_FIELD_RING_GREF, devpath));
-    info->rref = val ? strtoul(val, NULL, 10) : -1;
-
-    rc = 0;
-
-out:
-     GC_FREE;
-     return rc;
-}
-
 static LIBXL_DEFINE_UPDATE_DEVID(vkb)
 static LIBXL_DEFINE_DEVICE_FROM_TYPE(vkb)
 
 #define libxl__add_vkbs NULL
+#define libxl_device_vkb_list NULL
 #define libxl_device_vkb_compare NULL
 
-LIBXL_DEFINE_DEVID_TO_DEVICE(vkb)
-LIBXL_DEFINE_DEVICE_LIST(vkb)
 LIBXL_DEFINE_DEVICE_REMOVE(vkb)
 
 DEFINE_DEVICE_TYPE_STRUCT(vkb, VKBD,
     .skip_attach = 1,
     .dm_needed   = (device_dm_needed_fn_t)libxl__device_vkb_dm_needed,
-    .from_xenstore = (device_from_xenstore_fn_t)libxl__vkb_from_xenstore
 );
 
 /*
