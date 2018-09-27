20afc4b7b8c61c9dd3bfe8db3c0092239770e467
Andrii Anisov <andrii_anisov@epam.com>
2018-09-27 18:27:19 +0300

    irq: skip action avalability check for guest's IRQ
    An IRQ assigned to guest always has an action. This removes
    another odd check on guest IRQ path. Also getting an interrupt
    getting unknown interrupt is very unlikely on a non-debug
    platform.
    
    Signed-off-by: Andrii Anisov <andrii_anisov@epam.com>

--- xen/arch/arm/irq.c
+++ xen/arch/arm/irq.c
@@ -215,13 +215,6 @@ void do_IRQ(struct cpu_user_regs *regs, unsigned int irq, int is_fiq)
     spin_lock(&desc->lock);
     desc->handler->ack(desc);
 
-    if ( !desc->action )
-    {
-        printk("Unknown %s %#3.3x\n",
-               is_fiq ? "FIQ" : "IRQ", irq);
-        goto out;
-    }
-
     if ( test_bit(_IRQ_GUEST, &desc->status) )
     {
         struct irq_guest *info = irq_get_guest_info(desc);
@@ -273,6 +266,13 @@ void do_IRQ(struct cpu_user_regs *regs, unsigned int irq, int is_fiq)
         }
 
         goto out_no_end;
+    }
+
+    if ( unlikely(!desc->action) )
+    {
+        printk("Unknown %s %#3.3x\n",
+               is_fiq ? "FIQ" : "IRQ", irq);
+        goto out;
     }
 
     set_bit(_IRQ_PENDING, &desc->status);
