1b793f04daa8612aee7f57fed2e15cab4410e49a
Andrii Anisov <andrii_anisov@epam.com>
2018-10-03 20:21:38 +0300

    gic: separate ppi processing
    PPI are pcpu private, so their processing is not concurrent and do not
    need pcpu shared flags and correspondent lockings. So wipe out odd bits
    sets, checks and locks.
    
    Signed-off-by: Andrii Anisov <andrii_anisov@epam.com>

--- xen/arch/arm/gic.c
+++ xen/arch/arm/gic.c
@@ -773,8 +773,7 @@ void gic_interrupt(struct cpu_user_regs *regs, int is_fiq)
     do  {
         /* Reading IRQ will ACK it */
         irq = gic_hw_ops->read_irq();
-
-        if ( likely(irq >= 16 && irq < 1020) )
+        if ( likely(irq > 32 && irq < 1020) )
         {
             do_IRQ(regs, irq, is_fiq);
         }
@@ -789,6 +788,10 @@ void gic_interrupt(struct cpu_user_regs *regs, int is_fiq)
         else if ( irq < 16 )
         {
             do_sgi(regs, irq);
+        }
+        else if ( irq < 32 )
+        {
+            do_ppi(regs, irq);
         }
         else
         {
