d0a365d0420ebab3171df131286445ef75b6f8a6
Mirela Simonovic <mirela.simonovic@aggios.com>
2018-11-12 13:32:00 +0200

    xen/arm: Implement PSCI system suspend
    [ andr2000 Port to 4.12 ]
    
    call (virtual interface)
    To: <xen-devel@lists.xen.org>, <xen-devel@lists.xenproject.org>
    Cc: Tim Deegan <tim@xen.org>, Stefano Stabellini <sstabellini@kernel.org>,
    Wei Liu <wei.liu2@citrix.com>, <dm@aggios.com>, Konrad Rzeszutek Wilk <
    konrad.wilk@oracle.com>, George Dunlap <George.Dunlap@eu.citrix.com>,
    Andrew Cooper <andrew.cooper3@citrix.com>, Ian Jackson <
    ian.jackson@eu.citrix.com>, <saeed.nowshadi@xilinx.com>, Julien Grall <
    julien.grall@arm.com>, Jan Beulich <jbeulich@suse.com>, <
    stefano.stabellini@xilinx.com>, Mirela Simonovic <
    mirela.simonovic@aggios.com>
    
    The implementation consists of:
    -Adding PSCI system suspend call as new PSCI function
    -Trapping PSCI system_suspend HVC
    -Implementing PSCI system suspend call (virtual interface that allows
     guests to suspend themselves)
    
    The PSCI system suspend should be called by a guest from its boot
    VCPU. Non-boot VCPUs of the guest should be hot-unplugged using PSCI
    CPU_OFF call prior to issuing PSCI system suspend. Interrupts that
    are left enabled by the guest are assumed to be its wake-up interrupts.
    Therefore, a wake-up interrupt triggers the resume of the guest. Guest
    should resume regardless of the state of Xen (suspended or not).
    
    When a guest calls PSCI system suspend the respective domain will be
    suspended if the following conditions are met:
    1) Given resume entry point is not invalid
    2) Other (if any) VCPUs of the calling guest are hot-unplugged
    
    If the conditions above are met the calling domain is labeled as
    suspended and the calling VCPU is blocked. If nothing else wouldn't
    be done the suspended domain would resume from the place where it
    called PSCI system suspend. This is expected if processing of the PSCI
    system suspend call fails. However, in the case of success the calling
    guest should resume (continue execution after the wake-up) from the entry
    point which is given as the first argument of the PSCI system suspend
    call. In addition to the entry point, the guest expects to start within
    the environment whose state matches the state after reset. This means
    that the guest should find reset register values, MMU disabled, etc.
    Thereby, the context of VCPU should be 'reset' (as if the system is
    comming out of reset), the program counter should contain entry point,
    which is 1st argument, and r0/x0 should contain context ID which is 2nd
    argument of PSCI system suspend call. The context of VCPU is set
    accordingly when the PSCI system suspend is processed, so that nothing
    needs to be done on resume/wake-up path. However, in order to ensure that
    this context doesn't get overwritten by the scheduler when scheduling out
    this VCPU (would normally happen after the calling CPU is blocked), we need
    to check whether to return early from ctxt_switch_from().
    
    There are variables in domain structure to keep track of domain shutdown.
    One of existing shutdown reason is 'suspend' which this patch is using to
    track the suspend state of a domain. Those variables are used to determine
    whether to early return from ctxt_switch_from() or not.
    
    A suspended domain will resume after the Xen receives an interrupt which is
    targeted to the domain, unblocks the domain's VCPU, and schedules it in.
    When the VCPU is scheduled in, the VCPU context is already reset, and
    contains the right resume entry point in program counter that will be
    restored in ctxt_switch_to(). The only thing that needs to be done at this
    point is to clear the variables that marked the domain state as suspended.
    
    Signed-off-by: Mirela Simonovic <mirela.simonovic@xxxxxxxxxx>
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@xxxxxxxxxx>

---
+++ xen/include/asm-arm/suspend.h
@@ -0,0 +1,16 @@
+#ifndef __ASM_ARM_SUSPEND_H__
+#define __ASM_ARM_SUSPEND_H__
+
+int32_t domain_suspend(register_t epoint, register_t cid);
+
+#endif
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
