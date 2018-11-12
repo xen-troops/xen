176e63d25d7c872eb2e6fa06d1b27f8154bc509c
Mirela Simonovic <mirela.simonovic@aggios.com>
2018-11-12 13:32:00 +0200

    xen/arm: Implement PSCI SYSTEM_SUSPEND call (physical interface)
    saeed.nowshadi@xilinx.com>, Julien Grall <julien.grall@arm.com>, <
    stefano.stabellini@xilinx.com>, Mirela Simonovic <
    mirela.simonovic@aggios.com>
    
    PSCI system suspend function shall be invoked to finalize Xen suspend
    procedure. Resume entry point, which needs to be passed via 1st argument
    of PSCI system suspend call to the EL3, is hyp_resume. For now, hyp_resume
    is just a placeholder that will be implemented in assembly. Context ID,
    which is 2nd argument of system suspend PSCI call, is unused, as in Linux.
    
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@xilinx.com>

--- xen/arch/arm/psci.c
+++ xen/arch/arm/psci.c
@@ -25,6 +25,7 @@
 #include <asm/cpufeature.h>
 #include <asm/psci.h>
 #include <asm/acpi.h>
+#include <asm/suspend.h>
 
 /*
  * While a 64-bit OS can make calls with SMC32 calling conventions, for
@@ -66,6 +67,21 @@ void call_psci_cpu_off(void)
         panic("PSCI cpu off failed for CPU%d err=%d\n", smp_processor_id(),
               PSCI_RET(res));
     }
+}
+
+int call_psci_system_suspend(void)
+{
+#ifdef CONFIG_ARM_64
+    struct arm_smccc_res res;
+
+    /* 2nd argument (context ID) is not used */
+    arm_smccc_smc(PSCI_1_0_FN64_SYSTEM_SUSPEND, __pa(hyp_resume), &res);
+
+    return PSCI_RET(res);
+#else
+    /* not supported */
+    return 1;
+#endif
 }
 
 void call_psci_system_off(void)
