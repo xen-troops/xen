6580f0f39f38df21dc2b5aca3ec75974bd372166
Mirela Simonovic <mirela.simonovic@aggios.com>
2018-11-12 13:32:00 +0200

    xen/x86: Move freeze/thaw_domains into
    [ andr2000 Port to 4.12 ]
    
    common files
    To: <xen-devel@lists.xen.org>, <xen-devel@lists.xenproject.org>
    Cc: Tim Deegan <tim@xen.org>, Stefano Stabellini <sstabellini@kernel.org>,
    Wei Liu <wei.liu2@citrix.com>, <dm@aggios.com>, Konrad Rzeszutek Wilk <
    konrad.wilk@oracle.com>, George Dunlap <George.Dunlap@eu.citrix.com>,
    Andrew Cooper <andrew.cooper3@citrix.com>, Ian Jackson <
    ian.jackson@eu.citrix.com>, <saeed.nowshadi@xilinx.com>, Julien Grall <
    julien.grall@arm.com>, Jan Beulich <jbeulich@suse.com>, <
    stefano.stabellini@xilinx.com>, Mirela Simonovic <
    mirela.simonovic@aggios.com>
    
    These functions will be reused by suspend/resume support for ARM.
    
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@xilinx.com>

--- xen/common/domain.c
+++ xen/common/domain.c
@@ -1765,6 +1765,35 @@ int continue_hypercall_on_cpu(
     return 0;
 }
 
+
+void freeze_domains(void)
+{
+    struct domain *d;
+
+    rcu_read_lock(&domlist_read_lock);
+    /*
+     * Note that we iterate in order of domain-id. Hence we will pause dom0
+     * first which is required for correctness (as only dom0 can add domains to
+     * the domain list). Otherwise we could miss concurrently-created domains.
+     */
+    for_each_domain ( d )
+        domain_pause(d);
+    rcu_read_unlock(&domlist_read_lock);
+}
+
+void thaw_domains(void)
+{
+    struct domain *d;
+
+    rcu_read_lock(&domlist_read_lock);
+    for_each_domain ( d )
+    {
+        restore_vcpu_affinity(d);
+        domain_unpause(d);
+    }
+    rcu_read_unlock(&domlist_read_lock);
+}
+
 /*
  * Local variables:
  * mode: C
