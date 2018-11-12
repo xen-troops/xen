ed6660bf90dfd38160ec9ca6d80372f4c2a7cc91
Mirela Simonovic <mirela.simonovic@aggios.com>
2018-11-12 13:33:00 +0200

    xen/arm: Save/restore context on suspend/resume
    saeed.nowshadi@xilinx.com>, Julien Grall <julien.grall@arm.com>, <
    stefano.stabellini@xilinx.com>, Mirela Simonovic <
    mirela.simonovic@aggios.com>
    
    The context of CPU general purpose and system control registers
    has to be saved on suspend and restored on resume. This is
    implemented in hyp_suspend and before the return from hyp_resume
    function. The hyp_suspend is invoked just before the PSCI system
    suspend call is issued to the ATF. The hyp_suspend has to return a
    non-zero value so that the calling 'if' statement evaluates to true,
    causing the system suspend to be invoked. Upon the resume, context
    saved on suspend will be restored, including the link register.
    Therefore, after restoring the context the control flow will
    return to the address pointed by the saved link register, which
    is the place from which the hyp_suspend was called. To ensure
    that the calling 'if' statement doesn't again evaluate to true
    and initiate system suspend, hyp_resume has to return a zero value
    after restoring the context.
    Note that the order of saving register context into cpu_context
    structure has to match the order of restoring.
    Since the suspend/resume is supported only for arm64, we define
    a null cpu_context structure so arm32 could compile.
    
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@xilinx.com>

--- xen/include/asm-arm/suspend.h
+++ xen/include/asm-arm/suspend.h
@@ -1,8 +1,30 @@
 #ifndef __ASM_ARM_SUSPEND_H__
 #define __ASM_ARM_SUSPEND_H__
 
+#ifdef CONFIG_ARM_64
+struct cpu_context {
+    uint64_t callee_regs[12];
+    uint64_t sp;
+    uint64_t vbar_el2;
+    uint64_t vtcr_el2;
+    uint64_t vttbr_el2;
+    uint64_t tpidr_el2;
+    uint64_t mdcr_el2;
+    uint64_t hstr_el2;
+    uint64_t cptr_el2;
+    uint64_t hcr_el2;
+} __aligned(16);
+#else
+struct cpu_context {
+    uint8_t pad;
+};
+#endif
+
+extern struct cpu_context cpu_context;
+
 int32_t domain_suspend(register_t epoint, register_t cid);
 void hyp_resume(void);
+int32_t hyp_suspend(struct cpu_context *ptr);
 
 #endif
 
