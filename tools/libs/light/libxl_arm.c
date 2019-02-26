5f815e401b87dada7d4936183cf14326ead79dac
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2019-02-26 17:48:57 +0200

    xen/arm: Add support for GSX per-OSID irq counters
    Handle GSX irq separately from other guest irqs.
    Now Xen subscribes to GSX irq and performs all GSX related
    actions in an extra interrupt handler. If irq status register
    indicates a new event Xen reads all per-OSID irq counters and
    injects GSX irqs to required domains.
    
    Also pass vGSX OSID when creating domain.
    Having matching OSID <-> DomID in place, Xen can properly recognize
    to what domain to inject GSX irq.
    
    Based on the following patches from https://github.com/xen-troops/xen/pull/139:
    1. xen/arm: Add support for GSX per-OSID irq counters
       (provided by IMG)
    2. xen/arm: Rewrite injecting GSX irq logic V2
    3. xen/arm: Inject GSX irqs only if irq status register indicates a new event
       (provided by IMG)
    4. xen/arm: Pass vGSX OSID when creating domain
    
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

--- tools/libs/light/libxl_arm.c
+++ tools/libs/light/libxl_arm.c
@@ -101,6 +101,14 @@ int libxl__arch_domain_prepare_config(libxl__gc *gc,
         return ERROR_FAIL;
     }
 
+    if (d_config->num_vgsxs) {
+        libxl_device_vgsx *vgsx;
+
+        vgsx = &d_config->vgsxs[0];
+        config->arch.vgsx_osid = vgsx->osid;
+    } else
+        config->arch.vgsx_osid = 0;
+
     return 0;
 }
 
