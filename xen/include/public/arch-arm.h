xen-troops/xen

commit dba546c08188ddeb02846a4849b4d9de31507f8e

libxl: Creating device tree node for DomU guests
We create a "coord_suspend" node in the device tree for DomU guests. This
includes the IRQ# that will be triggered from Xen when DomU guests should
be suspended. We use virtual SPI 33, following the example of VPL011.

Signed-off-by: Joshua Kuhlmann <joshua.kuhlmann@aggios.com>

xen/include/public/arch-arm.h
@@ -454,6 +454,7 @@ typedef uint64_t xen_callback_t;
 #define GUEST_EVTCHN_PPI        31
 
 #define GUEST_VPL011_SPI        32
+#define GUEST_COORD_SUSPEND_SPI 33
 
 /* PSCI functions */
 #define PSCI_cpu_suspend 0

tools/libxl/libxl_arm.c
@@ -40,6 +40,12 @@ int libxl__arch_domain_prepare_config(libxl__gc *gc,
         vuart_enabled = true;
     }
 
+    /*
+     * Increment nr_spis to take into account coord_suspend IRQ, always enabled.
+     * We use the first unused interrupt, i.e. 33.
+     */
+    nr_spis += 1;
+
     for (i = 0; i < d_config->b_info.num_irqs; i++) {
         uint32_t irq = d_config->b_info.irqs[i];
         uint32_t spi;
@@ -628,6 +634,26 @@ static int make_vpl011_uart_node(libxl__gc *gc, void *fdt,
     return 0;
 }
 
+static int make_coord_susp_node(libxl__gc *gc, void *fdt)
+{
+    int res;
+    gic_interrupt intr;
+
+    res = fdt_begin_node(fdt, "coord_suspend");
+    if (res) return res;
+
+    res = fdt_property_compat(gc, fdt, 1, "xen,coord-suspend");
+    if (res) return res;
+
+    set_interrupt(intr, GUEST_COORD_SUSPEND_SPI, 0xf, DT_IRQ_TYPE_LEVEL_HIGH);
+
+    res = fdt_property_interrupts(gc, fdt, &intr, 1);
+    if (res) return res;
+
+    res = fdt_end_node(fdt);
+    return res;
+}
+
 static const struct arch_info *get_arch_info(libxl__gc *gc,
                                              const struct xc_dom_image *dom)
 {
@@ -903,6 +929,7 @@ next_resize:
         FDT( make_chosen_node(gc, fdt, !!dom->modules[0].blob, state, info) );
         FDT( make_cpus_node(gc, fdt, info->max_vcpus, ainfo) );
         FDT( make_psci_node(gc, fdt) );
+        FDT( make_coord_susp_node(gc, fdt) );
 
         FDT( make_memory_nodes(gc, fdt, dom) );
 

commit c93ee843ff64b7d6858642ef82b2ed12c20f0495

Fix coord suspend interrupt number for R-Car
R-Car platforms cannot use SPI 33 for coordinated suspend interrupt
as it is already in use. Select unused one: SPI 97 (#129).

Fixes: dba546c08188 ("libxl: Creating device tree node for DomU guests")

Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
Reviewed-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>

xen/include/public/arch-arm.h
@@ -454,7 +454,7 @@ typedef uint64_t xen_callback_t;
 #define GUEST_EVTCHN_PPI        31
 
 #define GUEST_VPL011_SPI        32
-#define GUEST_COORD_SUSPEND_SPI 33
+#define GUEST_COORD_SUSPEND_SPI 129
 
 /* PSCI functions */
 #define PSCI_cpu_suspend 0