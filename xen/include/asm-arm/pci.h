1e92661697d3b9a314ca3f70fbaf95a9a848e97e
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2020-09-16 10:33:07 +0300

    [RCAR-GEN3] xen/arm: Add support for R-Car Gen3 host bridge
    This is a non-ECAM host controller.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- xen/include/asm-arm/pci.h
+++ xen/include/asm-arm/pci.h
@@ -57,6 +57,9 @@ struct pci_config_window {
     uint8_t     busn_end;
     const struct pci_ecam_ops     *ops;
     void __iomem        *win;
+    /* R-Car */
+    /* TODO: is it bridge or config property? */
+    uint8_t root_bus_nr;
 };
 
 /* Forward declaration as pci_host_bridge and pci_ops depend on each other. */
