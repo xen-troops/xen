16f6882730221358dfd6862e9c1051bb20d46d34
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2020-09-17 16:30:09 +0300

    WIP pci=

--- tools/libs/light/libxl_pci.c
+++ tools/libs/light/libxl_pci.c
@@ -1555,6 +1555,16 @@ void libxl__device_pci_add(libxl__egc *egc, uint32_t domid,
     pas->starting = starting;
     pas->callback = device_pci_add_stubdom_done;
 
+/* FIXME: CONFIG_ARM is not defined for this project. */
+/*#ifdef CONFIG_ARM*/
+    rc = xc_assign_device(ctx->xch, domid, pcidev_encode_bdf(pcidev), /*flags*/0);
+    if (rc < 0) {
+        LOGED(ERROR, domid, "xc_assign_device failed");
+        rc = ERROR_FAIL;
+    }
+    goto out;
+/*#endif*/
+
     if (libxl__domain_type(gc, domid) == LIBXL_DOMAIN_TYPE_HVM) {
         rc = xc_test_assign_device(ctx->xch, domid, pcidev_encode_bdf(pcidev));
         if (rc) {
