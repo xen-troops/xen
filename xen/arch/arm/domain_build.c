548a0c8ad432f13fb101a889940554d080610532
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2020-11-09 10:37:28 +0200

    xen/arm: Do not map PCI ECAM space to Domain-0
    Host bridge controller's ECAM space is mapped into Domain-0's p2m,
    thus it is not possible to trap the same for vPCI via MMIO handlers.
    For this to work we need to not map those while constructing the domain.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- xen/arch/arm/domain_build.c
+++ xen/arch/arm/domain_build.c
@@ -1238,6 +1238,9 @@ static int __init map_range_to_domain(const struct dt_device_node *dev,
         }
     }
 
+    if ( need_mapping && (device_get_class(dev) == DEVICE_PCI) )
+        need_mapping = pci_host_bridge_need_mapping(d, dev, addr, len);
+
     if ( need_mapping )
     {
         res = map_regions_p2mt(d,
