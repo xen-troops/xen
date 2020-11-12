231f20074bcef3e0ce040dc98aca13b559c7ecc8
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2020-11-12 08:37:00 +0200

    [HACK][DO NOT MERGE] vpci/rcar: Make vPCI know DomD is hardware domain
    This also includes a HACK to detect if we run under QEMU.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- xen/arch/arm/pci/pci-host-zynqmp.c
+++ xen/arch/arm/pci/pci-host-zynqmp.c
@@ -23,6 +23,10 @@
 #include <xen/pci.h>
 #include <asm/pci.h>
 
+#include <xen/warning.h>
+
+extern bool pci_under_qemu;
+
 static const struct dt_device_match gen_pci_dt_match[] = {
     { .compatible = "xlnx,nwl-pcie-2.11",
       .data =       &pci_generic_ecam_ops },
@@ -33,6 +37,9 @@ static int gen_pci_dt_init(struct dt_device_node *dev, const void *data)
 {
     const struct dt_device_match *of_id;
     const struct pci_ecam_ops *ops;
+
+    pci_under_qemu = true;
+    warning_add("\n\nWARNING! ASSUMING QEMU\n\n\n");
 
     of_id = dt_match_node(gen_pci_dt_match, dev->dev.of_node);
     ops = (struct pci_ecam_ops *) of_id->data;
