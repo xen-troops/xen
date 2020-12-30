xen-troops/xen

commit 206bf976179ccedb628e9f056f0cef2ce76f0873

xen/arm: Do not map PCI ECAM space to Domain-0
Host bridge controller's ECAM space is mapped into Domain-0's p2m,
thus it is not possible to trap the same for vPCI via MMIO handlers.
For this to work we need to not map those while constructing the domain.

Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

xen/arch/arm/pci/pci-host-common.c
@@ -213,6 +213,28 @@ int pci_host_iterate_bridges(struct domain *d,
     }
     return 0;
 }
+
+bool pci_host_bridge_need_mapping(struct domain *d,
+                                  const struct dt_device_node *node,
+                                  u64 addr, u64 len)
+{
+    struct pci_host_bridge *bridge;
+    struct pci_dev *pdev = dev_to_pci((struct device *)&node->dev);
+
+    bridge = pci_find_host_bridge(pdev->seg, pdev->bus);
+    if ( unlikely(!bridge) )
+    {
+        printk(XENLOG_ERR "Unable to find PCI bridge for "PRI_pci"\n",
+               pdev->seg, pdev->bus, pdev->sbdf.dev, pdev->sbdf.fn);
+        return NULL;
+    }
+
+    if ( !bridge->ops->need_mapping )
+        return true;
+
+    return bridge->ops->need_mapping(d, bridge, addr, len);
+}
+
 /*
  * Local variables:
  * mode: C

xen/arch/arm/domain_build.c
@@ -1238,6 +1238,9 @@ static int __init map_range_to_domain(const struct dt_device_node *dev,
         }
     }
 
+    if ( need_mapping && (device_get_class(dev) == DEVICE_PCI) )
+        need_mapping = pci_host_bridge_need_mapping(d, dev, addr, len);
+
     if ( need_mapping )
     {
         res = map_regions_p2mt(d,

xen/arch/arm/pci/ecam.c
@@ -18,6 +18,7 @@
  */
 
 #include <xen/pci.h>
+#include <xen/sched.h>
 
 /*
  * Function to implement the pci_ops ->map_bus method.
@@ -51,6 +52,19 @@ static int pci_ecam_register_mmio_handler(struct domain *d,
     return 0;
 }
 
+static int pci_ecam_need_mapping(struct domain *d,
+                                 struct pci_host_bridge *bridge,
+                                 u64 addr, u64 len)
+{
+    struct pci_config_window *cfg = bridge->sysdata;
+
+    /* Only check for control domain which owns HW PCI host bridge. */
+    if ( !is_control_domain(d) )
+        return true;
+
+    return cfg->phys_addr != addr;
+}
+
 /* ECAM ops */
 const struct pci_ecam_ops pci_generic_ecam_ops = {
     .bus_shift  = 20,
@@ -59,6 +73,7 @@ const struct pci_ecam_ops pci_generic_ecam_ops = {
         .read                   = pci_generic_config_read,
         .write                  = pci_generic_config_write,
         .register_mmio_handler  = pci_ecam_register_mmio_handler,
+        .need_mapping           = pci_ecam_need_mapping,
     }
 };
 

xen/include/asm-arm/pci.h
@@ -72,6 +72,8 @@ struct pci_ops {
     int (*register_mmio_handler)(struct domain *d,
                                  struct pci_host_bridge *bridge,
                                  const struct mmio_handler_ops *ops);
+    int (*need_mapping)(struct domain *d, struct pci_host_bridge *bridge,
+                        u64 addr, u64 len);
 };
 
 /*
@@ -119,8 +121,17 @@ void __iomem *pci_ecam_map_bus(struct pci_host_bridge *bridge,
 int pci_host_iterate_bridges(struct domain *d,
                              int (*clb)(struct domain *d,
                                         struct pci_host_bridge *bridge));
+bool pci_host_bridge_need_mapping(struct domain *d,
+                                  const struct dt_device_node *node,
+                                  u64 addr, u64 len);
 #else   /*!CONFIG_HAS_PCI*/
 struct arch_pci_dev { };
 static inline void  pci_init(void) { }
+static inline bool pci_host_bridge_need_mapping(struct domain *d,
+                                                const struct dt_device_node *node,
+                                                u64 addr, u64 len)
+{
+    return true;
+}
 #endif  /*!CONFIG_HAS_PCI*/
 #endif /* __ARM_PCI_H__ */

commit 9865d5d58d08430d55e33141397bfc4db0faa547

xen/arm: Do not try to check non-existent PCI host's SBDF
At the moment we need to check if a PCI host bridge needs mappings,
e.g. during the Domain-0 creation, there is no pci_dev yet allocated for it,
thus we cannot match PCI host and its associated bridge by SBDF yet.
Use dt_device_node field for comparison instead.

Fixes: 206bf976179c ("xen/arm: Do not map PCI ECAM space to Domain-0")

Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

xen/arch/arm/pci/pci-host-common.c
@@ -234,20 +234,20 @@ bool pci_host_bridge_need_mapping(struct domain *d,
                                   u64 addr, u64 len)
 {
     struct pci_host_bridge *bridge;
-    struct pci_dev *pdev = dev_to_pci((struct device *)&node->dev);
 
-    bridge = pci_find_host_bridge(pdev->seg, pdev->bus);
-    if ( unlikely(!bridge) )
+    list_for_each_entry( bridge, &pci_host_bridges, node )
     {
-        printk(XENLOG_ERR "Unable to find PCI bridge for "PRI_pci"\n",
-               pdev->seg, pdev->bus, pdev->sbdf.dev, pdev->sbdf.fn);
-        return NULL;
+        if ( bridge->dt_node != node )
+            continue;
+
+        if ( !bridge->ops->need_mapping )
+            return true;
+
+        return bridge->ops->need_mapping(d, bridge, addr, len);
     }
-
-    if ( !bridge->ops->need_mapping )
-        return true;
-
-    return bridge->ops->need_mapping(d, bridge, addr, len);
+    printk(XENLOG_ERR "Unable to find PCI bridge for %s segment %d, addr %lx\n",
+           node->full_name, bridge->segment, addr);
+    return true;
 }
 
 /*