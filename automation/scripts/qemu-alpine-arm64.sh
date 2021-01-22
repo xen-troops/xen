xen-troops/xen

commit f58976544ff40d2488c3e2b05c765916eacf3e7c

automation: use test-artifacts/qemu-system-aarch64 instead of Debian's
Instead apt-get'ing Debian's qemu-system-aarch64, simply use the
provided QEMU binary under binaries.

Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>
Acked-by: Wei Liu <wl@xen.org>

automation/scripts/qemu-alpine-arm64.sh
@@ -3,8 +3,7 @@
 set -ex
 
 apt-get -qy update
-apt-get -qy install --no-install-recommends qemu-system-aarch64 \
-                                            u-boot-qemu \
+apt-get -qy install --no-install-recommends u-boot-qemu \
                                             u-boot-tools \
                                             device-tree-compiler \
                                             cpio \
@@ -69,10 +68,9 @@ echo "rc_verbose=yes" >> etc/rc.conf
 find . |cpio -H newc -o|gzip > ../xen-rootfs.cpio.gz
 cd ../..
 
-# XXX Silly workaround to get the following QEMU command to work
-# QEMU looks for "efi-virtio.rom" even if it is unneeded
-cp /usr/share/qemu/pvh.bin /usr/share/qemu/efi-virtio.rom
-qemu-system-aarch64 \
+# XXX QEMU looks for "efi-virtio.rom" even if it is unneeded
+curl -fsSLO curl -fsSLO https://github.com/qemu/qemu/raw/v5.2.0/pc-bios/efi-virtio.rom
+./binaries/qemu-system-aarch64 \
    -machine virtualization=true \
    -cpu cortex-a57 -machine type=virt \
    -m 1024 -display none \
@@ -107,7 +105,7 @@ rm -f smoke.serial
 set +e
 echo "  virtio scan; dhcp; tftpb 0x40000000 boot.scr; source 0x40000000"| \
 timeout -k 1 720 \
-qemu-system-aarch64 \
+./binaries/qemu-system-aarch64 \
     -machine virtualization=true \
     -cpu cortex-a57 -machine type=virt \
     -m 2048 -monitor none -serial stdio \

automation/scripts/qemu-smoke-arm64.sh
@@ -5,17 +5,16 @@ set -ex
 # Install QEMU
 export DEBIAN_FRONTENT=noninteractive
 apt-get -qy update
-apt-get -qy install --no-install-recommends qemu-system-aarch64 \
-                                            u-boot-qemu \
+apt-get -qy install --no-install-recommends u-boot-qemu \
                                             u-boot-tools \
                                             device-tree-compiler \
                                             busybox-static \
-                                            cpio
+                                            cpio \
+                                            curl
 
-# XXX Silly workaround to get the following QEMU command to work
-# QEMU looks for "efi-virtio.rom" even if it is unneeded
-cp /usr/share/qemu/pvh.bin /usr/share/qemu/efi-virtio.rom
-qemu-system-aarch64 \
+# XXX QEMU looks for "efi-virtio.rom" even if it is unneeded
+curl -fsSLO https://github.com/qemu/qemu/raw/v5.2.0/pc-bios/efi-virtio.rom
+./binaries/qemu-system-aarch64 \
    -machine virtualization=true \
    -cpu cortex-a57 -machine type=virt \
    -m 1024 -display none \
@@ -79,7 +78,7 @@ rm -f smoke.serial
 set +e
 echo "  virtio scan; dhcp; tftpb 0x40000000 boot.scr; source 0x40000000"| \
 timeout -k 1 240 \
-qemu-system-aarch64 \
+./binaries/qemu-system-aarch64 \
     -machine virtualization=true \
     -cpu cortex-a57 -machine type=virt \
     -m 1024 -monitor none -serial stdio \

commit 852b939326fe8c58df187ebac5e4c827b13edf99

automation: fix qemu-alpine-arm64.sh
Remove copy/paste error introduced by f58976544ff4 ("automation: use
test-artifacts/qemu-system-aarch64 instead of Debian's")

Fixes: f58976544ff4 ("automation: use test-artifacts/qemu-system-aarch64 instead of Debian's")
Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>

automation/scripts/qemu-alpine-arm64.sh
@@ -69,7 +69,7 @@ find . |cpio -H newc -o|gzip > ../xen-rootfs.cpio.gz
 cd ../..
 
 # XXX QEMU looks for "efi-virtio.rom" even if it is unneeded
-curl -fsSLO curl -fsSLO https://github.com/qemu/qemu/raw/v5.2.0/pc-bios/efi-virtio.rom
+curl -fsSLO https://github.com/qemu/qemu/raw/v5.2.0/pc-bios/efi-virtio.rom
 ./binaries/qemu-system-aarch64 \
    -machine virtualization=true \
    -cpu cortex-a57 -machine type=virt \