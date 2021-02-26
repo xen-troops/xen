31fcb921f4247a03bd80ab07f8a7d131313d887b
Anastasiia Lukianenko <anastasiia_lukianenko@epam.com>
2021-02-26 13:03:47 +0200

    tools/xenpcid: Make Libxl PCI get values from xenpcid instead of libxl side
    pci_multifunction_check needs to use "ls" and "lstat" commands to get
    information from sysfs.
    Add "is_exists" command processing to xenpcid daemon to make possible
    pci_multifunction_check read directories and check lstat by using xenpcid.
    
    Signed-off-by: Anastasiia Lukianenko <anastasiia_lukianenko@epam.com>

--- tools/include/xenpcid.h
+++ tools/include/xenpcid.h
@@ -37,6 +37,7 @@
 
 #define XENPCID_CMD_WRITE           "write"
 #define XENPCID_CMD_READ_HEX        "read_hex"
+#define XENPCID_CMD_EXISTS          "exists"
 #define XENPCID_CMD_PCI_PATH        "pci_path"
 #define XENPCID_CMD_PCI_INFO        "pci_info"
 
