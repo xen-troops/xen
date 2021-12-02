fc5443dc44b7f9d2452e49c8512f90432980101d
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2021-12-02 10:16:07 +0200

    [DOMD][SPIDER] Assign PCI devices to hardware domain during enumeration
    During PCI device enumeration those are not assigned to the hardware
    domain, thus rendering them unusable in it. This is because DomD only
    adds PCI devices to Xen, but doesn't assign them.
    This makes it possible to passthrough PCI devices to guest domains as
    during guest domain creation the PCI devices will get assigned, but
    there is no entity in the system which does the same for the hardware
    domain itself.
    
    Work this around by assigning PCI devices to the hardware domain as
    those got discovered.
    
    TODO: this patch has a workaround: as we do not yet support OSID for PCI
    devices all the devices are assigned to a single IPMMU context:
    
    (XEN) Assigning device 0000:00:00.0 to dom1
    (XEN) ipmmu: 0000:00:00.0: Reusing IPMMU context 1
    (XEN) ipmmu: 0000:00:00.0: Added master device (IPMMU /soc/iommu@eed40000 micro-TLBs 1)
    (XEN) PCI add device 0000:00:00.0
    (XEN) 000080007ffda9d8OF: iommu-map, using mask 00000000, id-base: 00000000, out-base: 00000020, length: 00000001, id: 00000100 -> 00000020
    (XEN) Assigning device 0000:01:00.0 to dom1
    (XEN) ipmmu: 0000:01:00.0: Reusing IPMMU context 1
    (XEN) ipmmu: 0000:01:00.0: Added master device (IPMMU /soc/iommu@eed40000 micro-TLBs 1)
    (XEN) PCI add device 0000:01:00.0
    
    To work this around for now do not call IPMMU's assign for the root
    complex (0000:00:00.0), so we can let the IPMMU context 1 be used by
    0000:01:00.0 which is to be passed through.
    This is possible due to the fact that root complex doesn't do any DMA
    operations itself.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- xen/drivers/passthrough/arm/ipmmu-vmsa.c
+++ xen/drivers/passthrough/arm/ipmmu-vmsa.c
@@ -1131,6 +1131,8 @@ static void ipmmu_free_root_domain(struct ipmmu_vmsa_domain *domain)
     xfree(domain);
 }
 
+static int ipmmu_deassign_device(struct domain *d, u8 devfn, struct device *dev);
+
 static int ipmmu_assign_device(struct domain *d, u8 devfn, struct device *dev,
                                uint32_t flag)
 {
@@ -1144,6 +1146,41 @@ static int ipmmu_assign_device(struct domain *d, u8 devfn, struct device *dev,
     if ( !to_ipmmu(dev) )
         return -ENODEV;
 
+#ifdef CONFIG_HAS_PCI
+    if ( dev_is_pci(dev) )
+    {
+        struct pci_dev *pdev = dev_to_pci(dev);
+        struct domain *old_d = pdev->domain;
+
+        printk(XENLOG_INFO "Assigning device %04x:%02x:%02x.%u to dom%d\n",
+               pdev->seg, pdev->bus, PCI_SLOT(devfn), PCI_FUNC(devfn),
+               d->domain_id);
+
+        /*
+         * XXX What would be the proper behavior? This could happen if
+         * pdev->phantom_stride > 0
+         */
+        if ( devfn != pdev->devfn )
+            ASSERT_UNREACHABLE();
+
+        list_move(&pdev->domain_list, &d->pdev_list);
+        pdev->domain = d;
+
+        /* dom_io is used as a sentinel for quarantined devices */
+        if ( d == dom_io )
+        {
+            int ret;
+
+            /*
+             * Try to de-assign: do not return error if it was already
+             * de-assigned.
+             */
+            ret = ipmmu_deassign_device(old_d, devfn, dev);
+
+            return ret == -ESRCH ? 0 : ret;
+        }
+    }
+#endif
     spin_lock(&xen_domain->lock);
 
     /*
@@ -1312,6 +1349,22 @@ static int ipmmu_add_device(u8 devfn, struct device *dev)
 
     /* Let Xen know that the master device is protected by an IOMMU. */
     dt_device_set_protected(dev_to_dt(dev));
+
+#ifdef CONFIG_HAS_PCI
+    if ( dev_is_pci(dev) )
+    {
+        struct pci_dev *pdev = dev_to_pci(dev);
+        int ret;
+
+        /* TODO: this check needs to be removed once PCI OSIDs are in place. */
+        if ( pdev->sbdf.sbdf != PCI_BDF(0, 0, 0) )
+        {
+            ret = ipmmu_assign_device(pdev->domain, devfn, dev, 0);
+            if (ret)
+                return ret;
+        }
+    }
+#endif
 
     dev_info(dev, "Added master device (IPMMU %s micro-TLBs %u)\n",
              dev_name(fwspec->iommu_dev), fwspec->num_ids);
