b6b6f0c73d56cd9c7976b230c31f8fb5d9a815ce
Rahul Singh <rahul.singh@arm.com>
2021-12-08 21:01:19 +0000

    xen/arm: Enable the existing MSI-X and MSI handlers support for ARM
    The existing MSI-X and MSI handlers support available for X86 is adapted
    for Arm.
    
    Enable existing MSIX handlers for accesses to the MSI-X message control
    field on the PCI configuration space, and traps for accesses to the
    memory region that contains the MSI-X table and PBA for ARM. This traps
    detect attempts from the guest to configure MSI-X interrupts and
    properly sets them up.
    
    Enable existing handlers for the MSI control, address, data and mask
    fields in order to detect accesses to them and setup the interrupts as
    requested by the guest.
    
    Signed-off-by: Rahul Singh <rahul.singh@arm.com>
    ---
    (cherry picked from commit de5f3cd06bea7950f30a25529a04a97fc9cc53a6 from
     the downstream branch poc/pci-passthrough from
     https://gitlab.com/xen-project/people/bmarquis/xen-arm-poc.git)
    
    changes since cherry-pick:
    * rebase
    * use stdint.h types s/u32/uint32_t/ and s/u64/uint64_t/
    * add iounmap call
    * rebase on top of
      b177892d2d0e ("vpci/msix: handle accesses adjacent to the MSI-X table")
    * s/guest_reg/guest_addr/
    * convert pci_find_cap_offset() call to pci_sbdf_t
    
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com> # rebase

--- xen/drivers/vpci/msix.c
+++ xen/drivers/vpci/msix.c
@@ -19,8 +19,10 @@
 
 #include <xen/msi.h>
 #include <xen/sched.h>
+#include <xen/vmap.h>
 #include <xen/vpci.h>
 
+#include <asm/io.h>
 #include <asm/p2m.h>
 
 static uint32_t cf_check control_read(
@@ -155,7 +157,12 @@ static bool access_allowed(const struct pci_dev *pdev, unsigned long addr,
 static struct vpci_msix_entry *get_entry(struct vpci_msix *msix,
                                          paddr_t addr)
 {
-    paddr_t start = vmsix_table_addr(msix->pdev->vpci, VPCI_MSIX_TABLE);
+    paddr_t start;
+
+    if ( is_hardware_domain(current->domain) )
+        start = vmsix_table_addr(msix->pdev->vpci, VPCI_MSIX_TABLE);
+    else
+        start = vmsix_guest_table_addr(msix->pdev->vpci, VPCI_MSIX_TABLE);
 
     return &msix->entries[(addr - start) / PCI_MSIX_ENTRY_SIZE];
 }
