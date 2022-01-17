f8e227959606e233ad9c8be279a91c39b52e332d
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2022-01-17 09:47:15 +0200

    ARM: ITS: implement quirks and add support for Renesas Gen4 ITS
    There are number of ITS implementations exist which are different from
    the base one which have number of functionalities defined as is
    "IMPLEMENTATION DEFINED", e.g. there may exist differences in cacheability,
    shareability and memory requirements and others. This requires
    appropriate handling of such HW requirements which are implemented as
    ITS quirks: GITS_IIDR (ITS Implementer Identification Register) is used to
    differentiate the ITS implementations and select appropriate set of
    quirks if any.
    
    As an example of such ITSes add quirk implementation for Renesas Gen4 ITS:
    - add possibility to override default cacheability and shareability
    settings used for ITS memory allocations;
    - add possibility to allocate memory used by ITS with specific memory
    requirements: introduce _x{z|m}alloc_whole_pages functions and free the
    memory with xfree as usual.
    
    The idea of the quirk implementation is inspired by the Linux kernel ITS
    quirk implementation [1].
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    
    [1] https://elixir.bootlin.com/linux/v5.16.1/source/drivers/irqchip/irq-gic-v3-its.c
    ---
    Cc: Andre Przywara <andre.przywara@arm.com>
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Julien Grall <julien@xen.org>
    Cc: Volodymyr Babchuk <Volodymyr_Babchuk@epam.com>
    Cc: Bertrand Marquis <bertrand.marquis@arm.com>
    Cc: Andrew Cooper <andrew.cooper3@citrix.com>
    Cc: George Dunlap <george.dunlap@citrix.com>
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Wei Liu <wl@xen.org>

--- xen/arch/arm/gic-v3-lpi.c
+++ xen/arch/arm/gic-v3-lpi.c
@@ -242,9 +242,9 @@ static int gicv3_lpi_allocate_pendtable(uint64_t *reg)
     if ( this_cpu(lpi_redist).pending_table )
         return -EBUSY;
 
-    val  = GIC_BASER_CACHE_RaWaWb << GICR_PENDBASER_INNER_CACHEABILITY_SHIFT;
+    val  = gicv3_its_get_cacheability() << GICR_PENDBASER_INNER_CACHEABILITY_SHIFT;
     val |= GIC_BASER_CACHE_SameAsInner << GICR_PENDBASER_OUTER_CACHEABILITY_SHIFT;
-    val |= GIC_BASER_InnerShareable << GICR_PENDBASER_SHAREABILITY_SHIFT;
+    val |= gicv3_its_get_shareability() << GICR_PENDBASER_SHAREABILITY_SHIFT;
 
     /*
      * The pending table holds one bit per LPI and even covers bits for
@@ -252,7 +252,8 @@ static int gicv3_lpi_allocate_pendtable(uint64_t *reg)
      * The GICv3 imposes a 64KB alignment requirement, also requires
      * physically contiguous memory.
      */
-    pendtable = _xzalloc(lpi_data.max_host_lpi_ids / 8, SZ_64K);
+    pendtable = _xmalloc_whole_pages(lpi_data.max_host_lpi_ids / 8, SZ_64K,
+                                     gicv3_its_get_memflags());
     if ( !pendtable )
         return -ENOMEM;
 
@@ -284,9 +285,9 @@ static int gicv3_lpi_set_proptable(void __iomem * rdist_base)
 {
     uint64_t reg;
 
-    reg  = GIC_BASER_CACHE_RaWaWb << GICR_PROPBASER_INNER_CACHEABILITY_SHIFT;
+    reg  = gicv3_its_get_cacheability() << GICR_PROPBASER_INNER_CACHEABILITY_SHIFT;
     reg |= GIC_BASER_CACHE_SameAsInner << GICR_PROPBASER_OUTER_CACHEABILITY_SHIFT;
-    reg |= GIC_BASER_InnerShareable << GICR_PROPBASER_SHAREABILITY_SHIFT;
+    reg |= gicv3_its_get_shareability() << GICR_PROPBASER_SHAREABILITY_SHIFT;
 
     /*
      * The property table is shared across all redistributors, so allocate
@@ -295,7 +296,8 @@ static int gicv3_lpi_set_proptable(void __iomem * rdist_base)
     if ( !lpi_data.lpi_property )
     {
         /* The property table holds one byte per LPI. */
-        void *table = _xmalloc(lpi_data.max_host_lpi_ids, SZ_4K);
+        void *table = _xmalloc_whole_pages(lpi_data.max_host_lpi_ids, SZ_4K,
+                                           gicv3_its_get_memflags());
 
         if ( !table )
             return -ENOMEM;
@@ -363,7 +365,6 @@ int gicv3_lpi_init_rdist(void __iomem * rdist_base)
     {
         table_reg &= ~GICR_PENDBASER_INNER_CACHEABILITY_MASK;
         table_reg |= GIC_BASER_CACHE_nC << GICR_PENDBASER_INNER_CACHEABILITY_SHIFT;
-
         writeq_relaxed(table_reg, rdist_base + GICR_PENDBASER);
     }
 
