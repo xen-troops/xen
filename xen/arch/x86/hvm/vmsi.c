2f99c5b5745471cd2f96f909ce669c1da43e8025
Rahul Singh <rahul.singh@arm.com>
2022-02-11 11:50:50 +0000

    xen/vpci: msix: change return value of vpci_msix_{read,write}
    Return value is different for the MMIO handler on ARM and x86
    architecture.
    
    To make the code common for both architectures change the return value
    of vpci_msix_{read, write} to bool. Architecture-specific return value
    will be handled in arch code.
    
    Issue-Id: SCM-3780
    Signed-off-by: Rahul Singh <rahul.singh@arm.com>
    Change-Id: I72cb005f8493a85826fa223693854dbae621f0d3
    ---
    (cherry picked from commit 3bc839e3f12887f18e8a12ba573b11f37096f13c from
     the downstream branch poc/pci-passthrough from
     https://gitlab.com/xen-project/people/bmarquis/xen-arm-poc.git)
    
    changes since cherry-pick:
    * rebase on top of the locking series
    * Rebase on top of
      b177892d2d0e ("vpci/msix: handle accesses adjacent to the MSI-X table")
    * rebase on top of the rwlock vpci series
    
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com> # rebase

--- xen/arch/x86/hvm/vmsi.c
+++ xen/arch/x86/hvm/vmsi.c
@@ -1032,7 +1032,10 @@ static int cf_check x86_msix_write(struct vcpu *v, unsigned long addr,
     const struct domain *d = v->domain;
     struct vpci_msix *msix = vpci_msix_find(d, addr);
 
-    return vpci_msix_write(msix, addr, len, data);
+    if( !vpci_msix_write(msix, addr, len, data) )
+        return X86EMUL_RETRY;
+
+    return X86EMUL_OKAY;
 }
 
 static int cf_check x86_msix_read(struct vcpu *v, unsigned long addr,
@@ -1041,7 +1044,10 @@ static int cf_check x86_msix_read(struct vcpu *v, unsigned long addr,
     const struct domain *d = v->domain;
     struct vpci_msix *msix = vpci_msix_find(d, addr);
 
-    return vpci_msix_read(msix, addr, len, data);
+    if ( !vpci_msix_read(msix, addr, len, data) )
+        return X86EMUL_RETRY;
+
+    return X86EMUL_OKAY;
 }
 
 static const struct hvm_mmio_ops vpci_msix_table_ops = {
