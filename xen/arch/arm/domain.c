634e097dfdf232f09c0b957f04e9135c76df53aa
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2023-09-14 02:10:25 +0300

    arm/vgic: provide late_init function
    vGIC ITS wants to map ITS registers into a guest, but at the moment of
    domain_vgic_init, p2m is not initialized yet, to so any call to p2m
    will fail. So, we need to postpone ITS initialization. This patch
    adds domain_late_init call-back, which can be called when domain
    construction is complete.
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    ---
    (cherry picked from commit 36d4a820d2b75f57e227da0c19bdd3d2a8a00e1b from the
     downstream branch https://github.com/lorc/xen/commits/msi_temp)

--- xen/arch/arm/domain.c
+++ xen/arch/arm/domain.c
@@ -883,6 +883,12 @@ int arch_domain_soft_reset(struct domain *d)
 void arch_domain_creation_finished(struct domain *d)
 {
     p2m_domain_creation_finished(d);
+
+    if ( domain_vgic_late_init(d) )
+    {
+        printk(XENLOG_INFO "Late vGIC initialization failed\n");
+        domain_crash(d);
+    }
 }
 
 static int is_guest_pv32_psr(uint32_t psr)
