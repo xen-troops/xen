a6173a4277245effeb001826533c5bcf1d8032d6
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2023-10-13 22:29:07 +0300

    [HACK][DOMD] force has_vpci to be true
    This is required to make DomD working.
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- xen/arch/arm/include/asm/domain.h
+++ xen/arch/arm/include/asm/domain.h
@@ -302,7 +302,7 @@ static inline void arch_vcpu_block(struct vcpu *v) {}
 
 #define arch_vm_assist_valid_mask(d) (1UL << VMASST_TYPE_runstate_update_flag)
 
-#define has_vpci(d) ( (d)->arch.has_vpci )
+#define has_vpci(d) ( true )
 
 struct arch_vcpu_io {
     struct instr_details dabt_instr; /* when the instruction is decoded */
