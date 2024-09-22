758c85159a6b69169bb6028de6f6c9d84050feb8
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2024-09-23 00:34:13 +0300

    xen: arch: scmi_smc: remove memory mapping for hardware domain
    Mapping for the hardware domain should be done during domain
    construciton as it is done for other domains.
    
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>
    Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- xen/arch/arm/domain_build.c
+++ xen/arch/arm/domain_build.c
@@ -1590,6 +1590,29 @@ int __init make_chosen_node(const struct kernel_info *kinfo)
     return res;
 }
 
+#ifdef CONFIG_SCMI_SMC
+static int __init mem_permit_access(struct domain *d, uint64_t addr, uint64_t len)
+{
+    int res, rc;
+    res = iomem_permit_access(d, paddr_to_pfn(addr),
+                              paddr_to_pfn(PAGE_ALIGN(addr + len - 1)));
+    if ( res )
+        return res;
+
+    res = map_mmio_regions(d, gaddr_to_gfn(addr), PFN_DOWN(len),
+            maddr_to_mfn(addr), p2m_mmio_direct_nc);
+    if ( res )
+    {
+        rc = iomem_deny_access(d, paddr_to_pfn(addr),
+                               paddr_to_pfn(PAGE_ALIGN(addr + len -1)));
+        if ( rc )
+            printk(XENLOG_ERR "Unable to deny iomem access , err = %d\n", rc);
+    }
+
+    return res;
+}
+#endif /* CONFIG_SCMI_SMC */
+
 static int __init handle_node(struct domain *d, struct kernel_info *kinfo,
                               struct dt_device_node *node,
                               p2m_type_t p2mt)
@@ -1741,6 +1764,12 @@ static int __init handle_node(struct domain *d, struct kernel_info *kinfo,
          */
         evtchn_allocate(d);
 
+#ifdef CONFIG_SCMI_SMC
+        res = mem_permit_access(kinfo->d, kinfo->d->arch.sci_channel.paddr,
+                                PAGE_SIZE);
+        if ( res )
+            return res;
+#endif
         /*
          * The hypervisor node should always be created after all nodes
          * from the host DT have been parsed.
